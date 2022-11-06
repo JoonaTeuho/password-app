6405ceb87b7afbaaafd6bd9d3be5b3bee1c45078
JoonaTeuho <joona.teuho@gmail.com>
2022-11-06 22:13:05 +0200

    added fetch function

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [pass, setPass] = useState('');
+
+  let length = 8
+  let upper = "on"
+  let lower = "on"
+  let special = "on"
+  let numbers = "on"
+
+  /* const getPassword = () => {
+    fetch(`https://passwordwolf.com/api/?length=${length}&upper=${upper}&lower=${lower}&special=${special}&numbers=${numbers}&repeat=1`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+    .catch(error => {
+      console.error(error)
+    });
+  }*/
+
+  const getPassword = async () => {
+    try {
+      const response = await fetch(
+        `https://passwordwolf.com/api/?length=${length}&upper=${upper}&lower=${lower}&special=${special}&numbers=${numbers}&repeat=1`
+        );
+      const data = await response.json();
+      const generatedPassword = await data[0].password;
+      setPass(generatedPassword);
+      
+      console.log(data)
+      console.log(generatedPassword)
+      console.log("Generated password: " + pass)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{pass}</Text>
+      <Button 
+      title="Create a password"
+      type="button"
+      onPress={ () => getPassword()}
+
+      />
       <StatusBar style="auto" />
-    </View>
-  );
+      </View>
+      );
 }
 
 const styles = StyleSheet.create({
